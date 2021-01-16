4f5e18db1b05ecb75375f4504b0da0f8ee07e9ee
solangepaz <paz_solange@hotmail.com>
2021-01-16 23:16:03 +0000

    EmployeeSearch08-01Prac

--- 08-CollectionsStreamsFilters/practices/practice1/EmployeeSearch08-01Prac/src/com/example/lambda/RoboMailTest01.java
+++ 08-CollectionsStreamsFilters/practices/practice1/EmployeeSearch08-01Prac/src/com/example/lambda/RoboMailTest01.java
@@ -1,5 +1,6 @@
 package com.example.lambda;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 
@@ -21,13 +22,51 @@ public class RoboMailTest01 {
     Predicate<Employee> salesEmployeesOver50 = 
       p -> p.getAge() >= 50 && p.getDept().equals("Sales");
     
-    System.out.println("\n==== RoboMail 01");
-    System.out.println("\n=== Sales Execs ===");
-    robo.mail(pl, salesExecutives); // Convert to stream and forEach
-    robo.text(pl, salesExecutives); // Convert to stream and forEach
+
+      System.out.println("\n==== RoboMail 01");
+      System.out.println("\n===  Sales Execs ===");
+      pl.stream()
+        .filter(e->e.getRole() == Role.EXECUTIVE)
+        .filter(e->e.getDept().equals("Sales"))
+        .forEach(e-> robo.roboMail(e));
+
+      pl.stream()
+        .filter(e->e.getRole() == Role.EXECUTIVE)
+        .filter(e->e.getDept().equals("Sales"))
+        .forEach(e-> robo.roboText(e));
+
+      System.out.println("\n\n=== All Sales ===");
+        pl.stream()
+        .filter(e->e.getDept().equals("Sales"))
+        .filter(e->e.getAge() > 50)
+        .forEach(e-> robo.roboMail(e));
+
+        pl.stream()
+        .filter(e->e.getDept().equals("Sales"))
+        .filter(e->e.getAge() > 50)
+        .forEach(e-> robo.roboText(e));
+  
+   /* pl.stream()
+      .filter(e->e.getAge() < 65)
+      .filter(e->e.getDept().equals("Eng"))
+      .filter(e->e.getGender() == Gender.MALE)
+      .forEach(e-> robo.roboMail(e));
+
+      System.out.println("\n=== Sales Execs ===");
+      pl.stream()
+      .filter(e->e.getAge() < 65)
+      .filter(e->e.getDept().equals("Eng"))
+      .filter(e->e.getGender() == Gender.MALE)
+      .forEach(e -> robo.roboText(e));
+*/
+        
+     
     
-    System.out.println("\n=== All Sales ===");
-    robo.mail(pl, salesEmployeesOver50); // Convert to stream and forEach
-    robo.text(pl, salesEmployeesOver50); // Convert to stream and forEach   
+ //   robo.mail(pl, salesExecutives); // Convert to stream and forEach
+ //   robo.text(pl, salesExecutives); // Convert to stream and forEach
+    
+  //  System.out.println("\n=== All Sales ===");
+  //  robo.mail(pl, salesEmployeesOver50); // Convert to stream and forEach
+  //  robo.text(pl, salesEmployeesOver50); // Convert to stream and forEach   
   }
 }
