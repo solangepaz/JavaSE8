d6ad3a897af262b8f9129d537d96872688b4e76b
solangepaz <paz_solange@hotmail.com>
2021-01-16 21:47:08 +0000

    Pet06-01Prac

--- 06-Interfaces/practices/practice1/Pet06-01Prac/src/com/example/Animal.java
+++ 06-Interfaces/practices/practice1/Pet06-01Prac/src/com/example/Animal.java
@@ -4,9 +4,10 @@ public abstract class Animal {
     
     protected int legs;
     
-    public Animal(int legs) {
+    protected Animal(int legs) {
         this.legs = legs;
     }
+
     
     public void walk() {
         System.out.println("This animal walks on " + legs + " legs.");
