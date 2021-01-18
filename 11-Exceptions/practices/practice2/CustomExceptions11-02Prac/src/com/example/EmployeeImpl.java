b679b92c1bbcb05de29c1e188bbd1a220c0f876d
solangepaz <paz_solange@hotmail.com>
2021-01-18 12:08:10 +0000

    CustomExceptions11-02Prac

--- 11-Exceptions/practices/practice2/CustomExceptions11-02Prac/src/com/example/EmployeeImpl.java
+++ 11-Exceptions/practices/practice2/CustomExceptions11-02Prac/src/com/example/EmployeeImpl.java
@@ -12,17 +12,40 @@ public class EmployeeImpl {
     EmployeeImpl() {
     }
 
-    public void add(Employee emp) {
+    public void add(Employee emp) throws InvalidOperationException {
+        if (employeeArray[emp.getId()] != null) {
+            throw new InvalidOperationException("Error adding employee, employee id already exists " + emp.getId());
+        }
+
+        try {
+            employeeArray[emp.getId()] = emp;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new InvalidOperationException("Error adding employee, id must be less than " + employeeArray.length);
+        }
 
     }
 
-    public void delete(int id) {
+    public void delete(int id) throws InvalidOperationException {
+        if (employeeArray[id] == null) {
+            throw new InvalidOperationException("Error deleting employee, " + id + " does not exist in array.");
+        }
+
+        try {
+            employeeArray[id] = null;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new InvalidOperationException(
+                    "Error deleting employee, id must be less than " + employeeArray.length);
+        }
 
     }
 
-    public Employee findById(int id) 
+    public Employee findById(int id) throws InvalidOperationException 
     {
-        return employeeArray[id];
+        try {
+            return employeeArray[id];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new InvalidOperationException("Error finding employee", e);
+        }
     }
     public Employee[] getAllEmployees() {
         List<Employee> emps = new ArrayList<>();
--- 11-Exceptions/practices/practice2/CustomExceptions11-02Prac/src/com/example/EmployeeTest.java
+++ 11-Exceptions/practices/practice2/CustomExceptions11-02Prac/src/com/example/EmployeeTest.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class EmployeeTest {
     
-    public static void main(String[] args)  {
+    public static void main(String[] args) throws InvalidOperationException {
         Employee e1 = new Employee(0, "bob", "builder", 1250.50f);
         EmployeeImpl emp = new EmployeeImpl();
         emp.add(e1);
@@ -15,11 +15,9 @@ public class EmployeeTest {
             System.out.println(employee + "\n");
         }
         
-      //emp.delete(7);
-       
+      emp.delete(5);
+      emp.delete(7);       
         
-                }          
-    
-    
+     }          
     
     }
