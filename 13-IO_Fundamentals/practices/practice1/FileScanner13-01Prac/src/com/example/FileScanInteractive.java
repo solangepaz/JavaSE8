fbfaf19b8cdb0779e78ec828b5c979ae34318d6b
solangepaz <paz_solange@hotmail.com>
2021-01-19 09:37:12 +0000

    FileScanner13-01Prac

--- 13-IO_Fundamentals/practices/practice1/FileScanner13-01Prac/src/com/example/FileScanInteractive.java
+++ 13-IO_Fundamentals/practices/practice1/FileScanner13-01Prac/src/com/example/FileScanInteractive.java
@@ -1,8 +1,9 @@
-package com.example;
+
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class FileScanInteractive {
@@ -24,5 +25,30 @@ public class FileScanInteractive {
 
     public static void main(String[] args) {
         // Your code goes here
+        if (args.length < 1) {
+            System.out.println("There are no arguments passed in!");
+            System.exit(-1);
+        }
+
+        FileScanInteractive fileScan = new FileScanInteractive();
+        String searchString = "";
+        String firstArg = args[0];
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
+            while (true) {
+            System.out.println("Enter search string or q to exit: ");
+            searchString = reader.readLine().trim();
+            
+            if (searchString.trim().equalsIgnoreCase("q"))
+                break;
+
+            int count = fileScan.countTokens(firstArg, searchString);
+            System.out.println("The search token appeared " + count + " times in the file");
+            }
+        }
+        catch(IOException e){
+            System.out.println(e.getMessage());
+            System.exit(-1);
+        }
     }
 }
