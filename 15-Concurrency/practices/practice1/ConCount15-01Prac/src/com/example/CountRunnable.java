8ad9e353ef51fe5e1139692321c811f8742980da
solangepaz <paz_solange@hotmail.com>
2021-01-20 10:01:08 +0000

    ConCount15-01Prac

--- 15-Concurrency/practices/practice1/ConCount15-01Prac/src/com/example/CountRunnable.java
+++ 15-Concurrency/practices/practice1/ConCount15-01Prac/src/com/example/CountRunnable.java
@@ -1,13 +1,21 @@
 package com.example;
 
 public class CountRunnable implements Runnable{
-  
-   //final int count;
-  //final String threadName;
+
+  final int count;
+  final String threadName;
   
   // Setup Constructor to initialize variables
+  public CountRunnable(int count, String threadName) {
+    this.count = count;
+    this.threadName = threadName;
+  }
+  
   
   public void run(){ 
     // Count with for loop
+    for (int i = 0; i < count; i++) {
+      System.out.println(threadName + ": " + i);
+    }
   }
 }
--- 15-Concurrency/practices/practice1/ConCount15-01Prac/src/com/example/Main.java
+++ 15-Concurrency/practices/practice1/ConCount15-01Prac/src/com/example/Main.java
@@ -7,10 +7,19 @@ public class Main {
 
   public static void main(String[] args) {
     // Setup Executor
+    ExecutorService es = Executors.newCachedThreadPool();
+
+    CountRunnable a = new CountRunnable(20, "A");
+    CountRunnable b = new CountRunnable(20, "B");
+    CountRunnable c = new CountRunnable(20, "C");
     
     // Submit Runnable
+    es.submit(a);
+    es.submit(b);
+    es.submit(c);
     
     // Showndown ExectuorService
+    es.shutdown();
 
   }
 }
